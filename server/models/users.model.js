71c1c073d0cc25a29a0250fa416118f061b5f876
Stephen Hang <stephenhang0@gmail.com>
2023-02-25 22:05:14 -0800

    signup

---
+++ server/models/users.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "A First Name is required"],
+        minLength: 2
+    },
+    lastName: {
+        type: String,
+        required: [true, "A Last Name is required"],
+        minLength: 2
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        validate: {
+            validator: val => /^([\w-\.]+@([\w-]+\.)+[\w-]+)?$/.test(val),
+            message: "Please enter a valid email"
+        },
+        minLength: 8
+    },
+    password: {
+        type: String,
+        required: [true, "A Password is required"],
+        minLength: [6, "Password should be at least 6 characters"]
+    }
+}, {timestamps: true})
+
+UserSchema.virtual('confirmPassword')
+.get(()=> this._confirmPassword)
+.set(value => this._confirmPassword = value)
+
+UserSchema.pre('validate', function(next) {
+    if (this.password !== this.confirmPassword) {
+        this.invalidate('confirmPassword', 'Password must match confirm password')
+    }
+    next()
+})
+
+UserSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 10)
+    .then(hash => {
+        this.password = hash
+        next()
+    })
+})
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
+require('dotenv').config()
 const cors = require('cors');
 const app = express();
 
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(cors({ origin: "http://localhost:3000",}),);
+app.use(cors({credentials: true,  origin: "http://localhost:3000"}),);
 
 require("./config/mongoose.config");
 require("./routes/jobs.routes")(app);
+require("./routes/users.routes")(app);
 
 
 app.listen(8000, () => {console.log("Listening at Port 8000")});
