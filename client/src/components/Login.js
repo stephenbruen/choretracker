c2025315d94be9e4fab909cf2b2ab96f596f6228
Stephen Hang <stephenhang0@gmail.com>
2023-02-25 23:04:05 -0800

    signup and login(cant get it to work)

---
+++ client/src/components/Login.js
@@ -0,0 +1,66 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useNavigate } from 'react-router-dom';
+
+
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [errMsg, setErrMsg] = useState({})
+
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:8000/api/login", {
+            email,
+            password
+        })
+        .then (res => {
+            console.log(res)
+            console.log(res.data)
+            console.log(email, password)
+        })
+    }
+
+    const emailHandler = (e) => {
+        setErrMsg("");
+        setEmail(e.target.value);
+    }
+    const passwordHandler = (e) => {
+        setErrMsg("");
+        setPassword(e.target.value);
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <Form onSubmit = {onSubmitHandler}>
+                <Form.Group as = {Row} className = "mb-3" controlId = "formHorizontalEmail">
+                        {errMsg.email? <p>{errMsg.email.message}</p> : null}
+                        <Form.Label column sm = {2}>
+                            Email
+                        </Form.Label>
+                        <Col sm = {10}>
+                            <Form.Control type='text' value={email} onChange={emailHandler} />
+                        </Col>
+                </Form.Group>
+
+                <Form.Group as = {Row} className = "mb-3" controlId = "formHorizontalPassword">
+                        {errMsg.password? <p>{errMsg.password.message}</p> : null}
+                        <Form.Label column sm = {2}>
+                            Password
+                        </Form.Label>
+                        <Col sm ={10}>
+                            <Form.Control type='text' value={password} onChange={passwordHandler} />
+                        </Col>
+                </Form.Group>
+                <Button variant = "primary" type = "submit">Login</Button>
+            </Form>
+        </div>
+    )
+}
+
+export default Login
--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,10 +1,11 @@
 const UsersController = require('../controllers/users.controller')
+const {authenticate } = require('../config/jwt.config');
 
 
 module.exports = (app) => {
     app.post('/api/register', UsersController.register)
     app.post('/api/login', UsersController.login)
     app.get('/api/logout', UsersController.logout)
-    app.get('/api/allUsers', UsersController.getAllUsers)
+    app.get('/api/allUsers', authenticate, UsersController.getAllUsers)
     
 }
