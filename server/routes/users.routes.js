372d326a7518d442f11d4f8059a3b8070004bfd9
Stephen Hang <stephenhang0@gmail.com>
2023-02-26 17:26:41 -0800

    added dashboard

--- server/routes/users.routes.js
+++ server/routes/users.routes.js
@@ -1,11 +1,11 @@
 const UsersController = require('../controllers/users.controller')
-const {authenticate } = require('../config/jwt.config');
+//const {authenticate } = require('../config/jwt.config');
 
 
 module.exports = (app) => {
     app.post('/api/register', UsersController.register)
     app.post('/api/login', UsersController.login)
     app.get('/api/logout', UsersController.logout)
-    app.get('/api/allUsers', authenticate, UsersController.getAllUsers)
+    app.get('/api/allUsers', UsersController.getAllUsers)
     
 }
