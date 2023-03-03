4fd9d48bb00ffb68cd7d4afc7a40968373481b2b
bruenstephen@gmail.com <stephenbruen@Stephens-MacBook-Pro.local>
2023-03-02 20:42:51 -0500

    presentation files used

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import AddJob from './components/AddJob';
 import './App.css'
 import HomePage from './components/HomePage';
-import Login from './components/Login';
-import Signup from './components/SignUp'
+import Login from './components/Login/Login';
+import Signup from './components/Signup/SignUp'
 import ViewJobs from './components/ViewJobs';
 import EditJob from './components/EditJob';
 import ViewUserJobs from './components/ViewUserJobs';
+
+
 
 function App() {
   return (
@@ -21,6 +23,7 @@ function App() {
           <Route element={<ViewJobs />} path='/view/:id' />
           <Route element = {<EditJob/>} path = '/edit/:id'/>
           <Route element={<ViewUserJobs />} path='/viewUserJob/:id' />
+          <Route element={<Signup />} path='/Signup'/>
         </Routes>
       </div>  
       </BrowserRouter>
